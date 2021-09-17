a1072c7dde41d7f8c71eeeb2a59dc1284da60f56
dennys <dennys.hsieh@gmail.com>
2021-09-17 22:58:09 +0800

    Fix #2, move configuration to ini files

---
+++ hMoney/Configuration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IniParser;
+using IniParser.Model;
+
+namespace hMoney
+{
+    public class Configuration
+    {
+        Globals gg = new Globals();
+        IniData data;
+
+        public void Init()
+        {
+            var parser = new FileIniDataParser();
+            data = parser.ReadFile("hMoney.ini");
+        }
+
+        public string GetDbPath ()
+        {
+            string dbPath = data["DB"]["DB_PATH"];
+
+            return dbPath;
+        }
+
+
+    }
+}
---
+++ hMoney/Globals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hMoney
+{
+    public class Globals
+    {
+        public String INI_FILE_NAME = "hMoney.ini";
+
+        public String getIniFileName()
+        {
+            return INI_FILE_NAME;
+        }
+    }
+}
