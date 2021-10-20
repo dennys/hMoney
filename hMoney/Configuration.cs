b058f1821904e6b7774bd4a0d7d55dc123904060
dennys <dennys.hsieh@gmail.com>
2021-10-21 07:48:15 +0800

    Add some exception handling.

--- hMoney/Configuration.cs
+++ hMoney/Configuration.cs
@@ -9,6 +9,12 @@ namespace hMoney
 {
     public class Configuration
     {
+        const String DEFAULT_UI_LANGUAGE = "en";
+        const String DEFAULT_UI_DATEFORMAT = "yyyy/mm/dd";
+        const String DEFAULT_UI_NUMBERFORMAT = "n2";
+        const String DEFAULT_UI_TREEACCOUNTFONTSIZE = "12";
+        const String DEFAULT_UI_FONTSIZE = "12";
+        const String DEFAULT_UI_AccountSummaryHeaderBackColor = "Color.Cornsilk";
         IniData data;
 
         public void Init()
@@ -30,7 +36,12 @@ namespace hMoney
         private void GenerateDefaultIni(FileIniDataParser parser)
         {
             IniData ini = new IniData();
-            ini["UI"]["Language"] = "en";
+            ini["UI"]["Language"] = DEFAULT_UI_LANGUAGE;
+            ini["UI"]["DateFormat"] = DEFAULT_UI_DATEFORMAT;
+            ini["UI"]["NumberFormat"] = DEFAULT_UI_NUMBERFORMAT;
+            ini["UI"]["TreeAccountFontSize"] = DEFAULT_UI_TREEACCOUNTFONTSIZE;
+            ini["UI"]["FontSize"] = DEFAULT_UI_FONTSIZE;
+            ini["UI"]["AccountSummaryHeaderBackColor"] = DEFAULT_UI_AccountSummaryHeaderBackColor;
             parser.WriteFile(Globals.INI_FILE_NAME, ini);
         }
 
@@ -43,37 +54,37 @@ namespace hMoney
         {
             this.Init();
             string language = data["UI"]["Language"];
-            return language;
+            return language ?? DEFAULT_UI_LANGUAGE;
         }
         public string GetDateFormat()
         {
             this.Init();
             string dateFormat = data["UI"]["DateFormat"];
-            return dateFormat;
+            return dateFormat ?? DEFAULT_UI_DATEFORMAT;
         }
         public string GetNumberFormat()
         {
             this.Init();
             string numberFormat = data["UI"]["NumberFormat"];
-            return numberFormat;
+            return numberFormat ?? DEFAULT_UI_NUMBERFORMAT;
         }
         public int GetTreeAccountFontSize()
         {
             this.Init();
-            int treeAccountFontSize = Convert.ToInt32(data["UI"]["TreeAccountFontSize"]);
-            return treeAccountFontSize;
+            String treeAccountFontSize = data["UI"]["TreeAccountFontSize"];
+            return Convert.ToInt32(treeAccountFontSize ?? DEFAULT_UI_TREEACCOUNTFONTSIZE);
         }
         public int GetFontSize()
         {
             this.Init();
-            int fontSize = Convert.ToInt32(data["UI"]["FontSize"]);
-            return fontSize;
+            String fontSize = data["UI"]["FontSize"];
+            return Convert.ToInt32(fontSize ?? DEFAULT_UI_FONTSIZE);
         }
         public Color GetAccountSummaryHeaderBackColor()
         {
             this.Init();
-            Color backColor = Color.FromName(data["UI"]["AccountSummaryHeaderBackColor"]);
-            return backColor;
+            String backColor = data["UI"]["AccountSummaryHeaderBackColor"];
+            return Color.FromName(backColor ?? DEFAULT_UI_AccountSummaryHeaderBackColor);
         }
     }
 }
