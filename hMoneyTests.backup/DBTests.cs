80a96b2ae46d43508e82a55e2584ac1e6369d0dd
dennys <dennys.hsieh@gmail.com>
2021-10-24 22:37:55 +0800

    Migrate hMoneyTest to dotNet 5.0

---
+++ hMoneyTests.backup/DBTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace hMoneyTests
+{
+    internal class DBTests
+    {
+
+    }
+}
